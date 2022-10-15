29f9b2b8192b36708c44011338b6699f162d087e
Vandivier <john@afterecon.com>
2022-10-15 17:59:38 -0400

    feat: log get cur user err

--- app/users/queries/getCurrentUser.ts
+++ app/users/queries/getCurrentUser.ts
@@ -1,13 +1,19 @@
 import { Ctx } from "blitz"
-import db from "db"
+import db, { User } from "db"
 
 export default async function getCurrentUser(_ = null, { session }: Ctx) {
-  if (!session.userId) return null
+  let user: null | Partial<User> = null
 
-  const user = await db.user.findFirst({
-    where: { id: session.userId as number },
-    select: { id: true, name: true, email: true, role: true },
-  })
+  try {
+    if (!session.userId) return null
+
+    user = await db.user.findFirst({
+      where: { id: session.userId as number },
+      select: { id: true, name: true, email: true, role: true },
+    })
+  } catch (err) {
+    console.log(`error in getCurrentUser: ${{ err }}`)
+  }
 
   return user
 }
