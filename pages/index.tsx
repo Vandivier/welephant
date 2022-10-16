a45809a7defa5a96a31d29fc59f862e6ec72e659
Vandivier <john@afterecon.com>
2022-10-16 14:42:32 -0400

    feat: accurate footer
    and welephant gh url

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,62 +93,25 @@ const Home: BlitzPage = () => {
               <strong>Dis</strong>
             </a>
           </p>
-          <pre>
-            <code>blitz generate all project name:string</code>
-          </pre>
-          <div style={{ marginBottom: "1rem" }}>(And select Yes to run prisma migrate)</div>
-          <div>
-            <p>
-              Then <strong>restart the server</strong>
-            </p>
-            <pre>
-              <code>Ctrl + c</code>
-            </pre>
-            <pre>
-              <code>blitz dev</code>
-            </pre>
-            <p>
-              and go to{" "}
-              <Link href="/projects">
-                <a>/projects</a>
-              </Link>
-            </p>
-          </div>
           <div className="buttons" style={{ marginTop: "5rem" }}>
             <a
-              className="button"
-              href="https://blitzjs.com/docs/getting-started?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Documentation
-            </a>
-            <a
               className="button-outline"
-              href="https://github.com/blitz-js/blitz"
+              href="https://github.com/Vandivier/welephant"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Github Repo
-            </a>
-            <a
-              className="button-outline"
-              href="https://discord.blitzjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Discord Community
+              Welephant Github Repo
             </a>
           </div>
         </main>
 
         <footer>
           <a
-            href="https://blitzjs.com?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
+            href="https://vandivier.github.io/not-johns-linktree/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Powered by Blitz.js
+            Built by John Vandivier
           </a>
         </footer>
 
