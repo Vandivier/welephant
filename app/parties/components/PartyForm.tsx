7be98a29ee8a1e1c2d0b632bd38a7ed82bb34462
Vandivier <john@afterecon.com>
2022-10-28 19:44:31 -0400

    feat: boolean fields
    better party form

--- app/parties/components/PartyForm.tsx
+++ app/parties/components/PartyForm.tsx
@@ -1,12 +1,17 @@
-import { Form, FormProps } from "app/core/components/Form";
-import { LabeledTextField } from "app/core/components/LabeledTextField";
-import { z } from "zod";
-export { FORM_ERROR } from "app/core/components/Form";
+import { Form, FormProps } from "app/core/components/Form"
+import LabeledBooleanField from "app/core/components/LabeledBooleanField"
+import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { z } from "zod"
+export { FORM_ERROR } from "app/core/components/Form"
 
 export function PartyForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
       <LabeledTextField name="name" label="Name" placeholder="Name" />
+
+      <LabeledBooleanField name="hasGiftAssignments" label="Has Gift Assignments" default={true} />
+      <LabeledBooleanField name="hasGiftStealing" label="Has Gift Stealing" />
+      <LabeledTextField name="notes" label="Notes" placeholder="Notes" />
     </Form>
-  );
+  )
 }
--- app/parties/mutations/createParty.ts
+++ app/parties/mutations/createParty.ts
@@ -1,18 +1,28 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { z } from "zod";
+import { resolver } from "@blitzjs/rpc"
+import db, { Prisma } from "db"
+import { z } from "zod"
 
 const CreateParty = z.object({
+  hasGiftAssignments: z.boolean(),
+  hasGiftStealing: z.boolean(),
   name: z.string(),
-});
+  notes: z.string(),
+})
 
 export default resolver.pipe(
   resolver.zod(CreateParty),
   resolver.authorize(),
-  async (input) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const party = await db.party.create({ data: input });
+  async (input, ctx) => {
+    ctx.session.$isAuthorized()
 
-    return party;
+    const userId = ctx.session.userId
+    const augmented: Prisma.PartyCreateInput | Prisma.PartyUncheckedCreateInput = {
+      ...input,
+      ...{ userId },
+    }
+
+    const party = await db.party.create({ data: augmented })
+
+    return party
   }
-);
+)
