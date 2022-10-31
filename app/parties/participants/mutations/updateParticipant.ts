ee3abdf8d1a8130afaf278347a503793f3d6a2ed
Vandivier <john@afterecon.com>
2022-10-30 20:42:32 -0400

    feat: link user to participant

--- app/parties/participants/mutations/updateParticipant.ts
+++ app/parties/participants/mutations/updateParticipant.ts
@@ -1,5 +1,5 @@
 import { resolver } from "@blitzjs/rpc"
-import db, { ParticipantStatus, Prisma } from "db"
+import db, { Participant, ParticipantStatus, Prisma, User } from "db"
 import { z } from "zod"
 
 const UpdateParticipant = z.object({
@@ -21,11 +21,26 @@ export default resolver.pipe(
       : ParticipantStatus.DECLINED
     const massaged = { ...dataToKeep, status }
 
-    if (data.isGifter && !data.email)
-      throw new Error("Email must be provided to register as a gifter.")
+    if (data.isGifter) {
+      if (data.email) {
+        const userToAssociate = await db.user.findUnique({ where: { email: data.email } })
+        let maybeAfterAssociation: Participant | null = null
+        const participant = await db.participant.update({ where: { id }, data: massaged })
+
+        if (userToAssociate) {
+          maybeAfterAssociation = await db.participant.update({
+            where: { id },
+            data: { User: { connect: { id: userToAssociate.id } } },
+          })
+        }
+
+        return maybeAfterAssociation || participant
+      } else {
+        throw new Error("Email must be provided to register as a gifter.")
+      }
+    }
 
     const participant = await db.participant.update({ where: { id }, data: massaged })
-
     return participant
   }
 )
